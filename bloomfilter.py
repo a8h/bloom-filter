e762ceb02ae4a953daba4d53324abb28e9cede16
Arun <aa524@cornell.edu>
2019-12-26 16:24:01 +0100

    Refactoring and adding api methods

--- bloomfilter.py
+++ bloomfilter.py
@@ -3,21 +3,37 @@ import hashlib
 filter = [False] * 16
 test_message = "Hello World"
 
-def hexdigestToBits(hex_digest, filter_size):
+#Returns the hash for the filter
+def _generate_hash(hex_digest, filter_size):
     return int(str(hex_digest), 16) % filter_size
 
-def generate_hexdigests(test_message):
+#TODO: Allow usage of more than two digests
+def _generate_hexdigests(message):
     m = hashlib.sha256()
-    m.update(test_message)
+    m.update(message)
     x = hashlib.md5()
-    x.update(test_message)
+    x.update(message)
     return [m.hexdigest(), x.hexdigest()]
-    
 
-truebits = [hexdigestToBits(hd, len(filter)) for hd in generate_hexdigests(test_message)]
+def _generate_hashes(message):
+    return [_generate_hash(hd, len(filter)) for hd in _generate_hexdigests(message)]
 
-for tb in truebits:
-    filter[tb] = True
 
-print(filter)
+def addMessage(message):
+    truebits = _generate_hashes(message)
 
+    for tb in truebits:
+        filter[tb] = True
+
+def checkMessage(message):
+    truebits = _generate_hashes(message)
+    return all([filter[hash] for hash in truebits])
+
+
+def test():
+    addMessage(test_message)
+    print(filter)
+    print(checkMessage(test_message))
+    print(checkMessage("hello"))
+
+test()
